553e4aee72cf4c084e045ac72e8e15681afe3000
KING.JS <48418774+RONALDO445@users.noreply.github.com>
2020-06-08 18:08:43 -0400

    Update Usuário.js

--- Usuário.js
+++ Usuário.js
@@ -1,1 +1,19 @@
-console.log('usuario.js 200:on');
+console.log('usuario.js 200:ok')
+
+function removeAdd(){		
+var node = document.getElementById("adContainer"); 
+if (node.parentNode) { node.parentNode.removeChild(node); }
+var elemento = document.getElementById("smallAdContainer"); 
+if (elemento.parentNode) { elemento.parentNode.removeChild(elemento); }		
+}
+
+
+function img(){
+var imgs=document.createElement('img')		imgs.src='https://images.app.goo.gl/djQTNgRy6qt2UABJ7';
+imgs{
+			heigth:100px;
+			width:100px;
+		 }
+}
+removeAdd();
+img();
